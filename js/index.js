3834fa0301c45f7d41e301cd52743d716106c7eb
Mikael Moura <mikael.moura79@gmail.com>
2023-04-07 20:18:44 -0300

    formatting code

--- js/index.js
+++ js/index.js
@@ -1,19 +1,21 @@
 const email = document.getElementById('email-input');
-const errorMessage = document.getElementById("error-message");
-const iconError = document.getElementById("icon-error");
+const errorMessage = document.getElementById('error-message');
+const iconError = document.getElementById('icon-error');
 
 
 function submitEmail(){
     const emailValue = email.value
-    if(!emailValue || !emailValue.includes('@') 
-    || !emailValue.includes('.') || emailValue.length <= 4) {
-        errorMessage.innerHTML = "Please provide valid email"
+    if(!emailValue ||
+        !emailValue.includes('@') ||
+        !emailValue.includes('.') ||
+         emailValue.length <= 4) {
+        errorMessage.innerHTML = 'Please provide valid email'
         errorMessage.style.color = 'red'
         iconError.style.display = 'block'
     } else {
-        errorMessage.innerHTML = "Email successfully sent"
+        errorMessage.innerHTML = 'Email successfully sent'
         errorMessage.style.color = 'green'
-        email.value = "" 
+        email.value = ' ' 
         iconError.style.display = 'none'
     }
 }
